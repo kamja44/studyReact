7136e7841051a0c261c94f9da032941edc275100
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-10-13 17:31:49 +0900

    Movie App
    npm i react-router-dom@5.3.0
    
    no setting router yet

--- react-for-biggener/src/App.js
+++ react-for-biggener/src/App.js
@@ -1,41 +1,7 @@
 import {useState, useEffect} from "react";
-
+import Movie from "./components/Movie.js";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const onChange = (event) => {
-    setMoney(() => event.target.value);
-  }
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) =>{
-        setCoins(json);
-        setLoading(false);
-      });
-  },[])
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
-      {loading ? <strong>Loading...</strong> : null }
-      {loading ? null : <h3>
-        I Have 
-        <input 
-          placeholder="input your money"
-          onChange={onChange}
-          type="number"
-          value={money}></input>$
-      </h3>}
-      <select>
-        {coins.map((coin) => (
-          <option key={coin.id}>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD / You Can Buy {money/coin.quotes.USD.price} Coins!
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+  return null;
 }
 
 export default App;
