32e654d71fc41cc706eaf02a90b84713dcadb02b
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-10-13 11:42:25 +0900

    useEffect2
    useEffect(함수, 배열)
    배열에 state 선언 시 state가 변경되면 useEffect의 함수를 실행한다.

--- react-for-biggener/src/App.js
+++ react-for-biggener/src/App.js
@@ -3,11 +3,27 @@ import {useState, useEffect} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((current) => current + 1);
-  console.log("i run all the time");
-  useEffect(() => console.log("call the API"), []); 
+  const onChange = (event) => setKeyword(event.target.value);
+  // useEffect의 첫 번째 argument는 단 한번만 실행된다.
+  useEffect(() => console.log("Only Once"), []); 
+  useEffect(() => {
+    console.log("keyword Changed");
+  }, [keyword]); // [keyword]가 변경될 때 코드를 실행한다.
+  useEffect(()=>{
+    console.log("Counter changed");
+  }, [counter]);
+  useEffect(()=>{
+    console.log("keyword & counter changed");
+  }, [keyword, counter]);
   return (
     <div>
+      <input 
+      value={keyword} 
+      onChange={onChange} 
+      type="text" 
+      placeholder="Search here..."  />
       <h1>{counter}</h1>
       <Button text={"Click me"} onClick={onClick} />
     </div>
