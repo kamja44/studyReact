fc5db783d0321ecfa97b36627e7c19b20aff594e
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-10-13 15:17:29 +0900

    Coin Tracker
    coin Tracker

--- react-for-biggener/src/App.js
+++ react-for-biggener/src/App.js
@@ -1,38 +1,39 @@
-import Button from "./Button";
 import {useState, useEffect} from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onClick = (index) => {
-    setToDos(toDos.filter((item, toDoIndex) => index !== toDoIndex));
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === ""){
-      return;
-    }
-    setToDos((currentArray) => [toDo,...currentArray]); // ...currentArray <- currentArray 배열의 요소를 꺼낸다.
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const onChange = (event) => {
+    setMoney(() => event.target.value);
+  }
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) =>{
+        setCoins(json);
+        setLoading(false);
+      });
+  },[])
   return (
     <div>
-      <h1>To Do List ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The Coins! ({coins.length}) </h1>
+      {loading ? <strong>Loading...</strong> : null }
+      <h3>
+        I Have 
         <input 
-          onChange={onChange} 
-          value={toDo} 
-          type="text" 
-          placeholder="Write your to do..." 
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}>{item}  <button onClick={() => onClick(index)}>❌</button></li>)}
-      </ul>
-      {/* map() <- 하나의 array에 있는 item(요소)을 지정한 요소로 바꿔준 후 새로운 array로 반환한다. */}
+          placeholder="input your money"
+          onChange={onChange}
+          type="number"
+          value={money}></input>$
+      </h3>
+      <select>
+        {coins.map((coin) => (
+          <option>
+            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD / You Can Buy {money/coin.quotes.USD.price} Coins!
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
